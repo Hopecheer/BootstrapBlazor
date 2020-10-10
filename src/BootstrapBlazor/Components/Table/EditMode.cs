36b67a24388a03e358f73bea2eb181883a2646d5
Argo-PD <argo@163.com>
2020-10-10 10:39:37 +0800

    feat: 增加 Table 组件编辑模式枚举类型

---
+++ src/BootstrapBlazor/Components/Table/EditMode.cs
@@ -0,0 +1,18 @@
+﻿namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Table 组件编辑模式枚举类型
+    /// </summary>
+    public enum EditMode
+    {
+        /// <summary>
+        /// 弹窗式编辑模式
+        /// </summary>
+        PopupEditForm,
+
+        /// <summary>
+        /// 编辑表单模式
+        /// </summary>
+        EditForm
+    }
+}
